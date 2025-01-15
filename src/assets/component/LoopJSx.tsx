91f3c34038f71cc42c15ab4eb4e0c9cd45c11ff4
Rohit Rawat <rohitrawat559@gmail.com>
2025-01-15 10:14:23 +0530

    ADD : data

--- src/assets/component/LoopJSx.tsx
+++ src/assets/component/LoopJSx.tsx
@@ -1,17 +1,12 @@
 import React from 'react'
+import {employees} from '../../../src/utils/data'
 
 function LoopJSx() {
-    const employees =[{name: 'rohit', age:'20',email: 'rohit@gmail.com', designation:'admin',id :'001'},
-        {name: 'rahul', age:'21', email: 'rahul@gmail.com', designation:'manager',id :'002'},
-        {name: 'ramesh', age:'22', email: 'ramesh@gmail.com', designation:'developer',id :'003'},
-        {name: 'ramesh', age:'22', email: 'ramesh@gmail.com', designation:'developer',id :'004'},
-        {name: 'ramesh', age:'22', email: 'ramesh@gmail.com', designation:'developer',id :'005'},
-        {name: 'ramesh', age:'22', email: 'ramesh@gmail.com', designation:'developer',id :'006'},
-    ]
+    
   return (
     <div>
         <h1> List of Emplyees</h1>
-        <table border='1'>
+        <table>
             <thead>
                 <tr>
                     <td>Id</td>
---
+++ src/utils/data.tsx
@@ -0,0 +1,7 @@
+export const employees =[{name: 'rohit', age:'20',email: 'rohit@gmail.com', designation:'admin',id :'001'},
+    {name: 'rahul', age:'21', email: 'rahul@gmail.com', designation:'manager',id :'002'},
+    {name: 'ramesh', age:'22', email: 'ramesh@gmail.com', designation:'developer',id :'003'},
+    {name: 'ramesh', age:'22', email: 'ramesh@gmail.com', designation:'developer',id :'004'},
+    {name: 'ramesh', age:'22', email: 'ramesh@gmail.com', designation:'developer',id :'005'},
+    {name: 'ramesh', age:'22', email: 'ramesh@gmail.com', designation:'developer',id :'006'},
+]
